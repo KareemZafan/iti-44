2a4db8803510dcbbeee34ce35851fbe0f4b10a91
KareemZafan <kareemm.zafan@gmail.com>
2024-01-21 10:59:10 +0200

    Adding my feature on my branch

--- src/main.c
+++ src/main.c
@@ -4,11 +4,16 @@ int incrementBy3(int value){
   return value + 3 ; 
 }
 
+int add(int a , int b ){
+  return a + b  ; 
+}
+
 int main(void){
 
  printf("Hello, World); 
 
- printf("%d",incrementBy3(5)) // it prints 8 
+ printf("%d",incrementBy3(5));  // it prints 8 ,
+ printf("%d + %d = %d", 3,4, add(3,4)); 
 
 return 0 ; 
 }
