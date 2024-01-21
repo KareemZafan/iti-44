d5f0c01e017618d5015b0fba27ed8ffea95fe9fb
KareemZafan <kareemm.zafan@gmail.com>
2024-01-21 11:34:36 +0200

    apply review commnets from Mario

--- src/calc.c
+++ src/calc.c
@@ -1,23 +1,29 @@
 
 
+
+
+// it adds to decimals values 
 double add(double a , double b){
 
 return a + b ; 
 
 }
 
+// it subtracts to decimals values 
 double sub(double a , double b){
 
 return a - b ; 
 
 }
 
+// it multiplies to decimals values 
 double mul(double a , double b){
 
 return a * b ; 
 
 }
 
+// it divides to decimals values 
 double div(double a , double b){
  if(b == 0) return null; 
 return a / b ; 
