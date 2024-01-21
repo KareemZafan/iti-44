5d54d3dd0839f6cf834bd66011bbe3b0aa1a7988
Kareem Mohamed <61213045+KareemZafan@users.noreply.github.com>
2024-01-21 10:03:14 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+int incrementBy3(int value){
+  return value + 3 ; 
+}
 
 int main(void){
 
  printf("Hello, World); 
+
+ printf("%d",incrementBy3(5)) // it prints 8 
 
 return 0 ; 
 }
